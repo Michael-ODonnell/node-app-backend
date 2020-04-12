d7e9720767f0ca12e0532a6d0ba043a98550944b
Michael O'Donnell <reg+development@roadtodawn.co.uk>
2020-04-12 13:59:38 +0100

    API Params example

--- web/routes/users.js
+++ web/routes/users.js
@@ -6,4 +6,10 @@ usersRoute.get('/', function (req, res) {
     res.send('Users home page');
 });
 
+// define the home page route
+usersRoute.get('/:uid', function (req, res) {
+    const uid = req.params.uid;
+    res.send(`User ${uid}`);
+});
+
 module.exports = usersRoute;
